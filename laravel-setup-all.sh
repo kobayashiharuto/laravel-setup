52aa3fb832f7313afbc4df21674630a39b80f847
kl <datesaver1234@gmail.com>
2022-03-02 00:12:32 +0900

    完全自動化

--- laravel-setup-all.sh
+++ laravel-setup-all.sh
@@ -9,12 +9,11 @@ systemctl start apache2
 
 # apache設定
 sed -i -e "s@var/www/@var/www/$LARAVEL_APP_NAME/public/@g" /etc/apache2/apache2.conf
-sed -i -e "s@DocumentRoot /var/www/html@DocumentRoot $LARAVEL_APP_NAME" /etc/apache2/apache2.conf
-service restart apache2
+sed -i -e "s@DocumentRoot /var/www/html@DocumentRoot /var/www/$LARAVEL_APP_NAME/public@g" /etc/apache2/sites-available/000-default.conf
 
 # php導入
 apt install -y software-properties-common # add-apt-repositoryのインストール
-LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php # php のリポジトリを追加
+echo "\n" | LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php # php のリポジトリを追加
 apt install -y php8.1 php8.1-intl php8.1-dom php8.1-curl libapache2-mod-php8.1 php8.1-mbstring php8.1-mysql php8.1-gd php8.1-fpm # php系をインストール
 
 # composer導入
@@ -26,7 +25,13 @@ php composer-setup.php --install-dir=/usr/local/bin --filename=composer; # compo
 echo 2 | update-alternatives --config php
 php --ini | grep "Loaded Configuration File" | sed -e 's/.*: //g' | xargs sed -i -e "s/;extension=curl/extension=curl/g"
 
+# apacheの再起動
+systemctl restart apache2
+
 # Laravelプロジェクトの作成
-composer create-project laravel/laravel --prefer-dist $LARAVEL_APP_NAME
-cd $LARAVEL_APP_NAME
-composer install
+cd /var/www \
+  && composer create-project laravel/laravel --prefer-dist $LARAVEL_APP_NAME \
+  && cd $LARAVEL_APP_NAME \
+  && chmod 777 bootstrap/cache -R \
+  && chmod 777 storage -R \
+  && composer install
